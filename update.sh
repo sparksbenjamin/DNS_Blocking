07a6ea179ee28c7c1bc92a88601d5fb18d034fae
Ben Sparks <sparksbenjamin@gmail.com>
2026-01-11 01:56:12 -0500

    Add custom categories build script to update.sh

--- update.sh
+++ update.sh
@@ -5,5 +5,10 @@ python scripts/build_services.py
 echo "Running Services Script"
 python scripts/service_list_generator.py
 
+
+echo "Custom Cats"
+./scripts/build_categories.sh
+
+
 echo "Running Tunneling Script"
 python scripts/tunneling.py
