54b77cf405e4ae28e2f0c147120a0e214977c517
Ben Sparks <sparksbenjamin@gmail.com>
2026-02-03 13:37:58 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 
-echo "Building services.json"
-python scripts/build_services.py
+#echo "Building services.json"
+#python scripts/build_services.py
 
-echo "Running Services Script"
-python scripts/service_list_generator.py
-
-
-echo "Custom Cats"
-./scripts/build_categories.sh
+#echo "Running Services Script"
+#python scripts/service_list_generator.py
 
 
+#echo "Custom Cats"
+#./scripts/build_categories.sh
+
+echo "Run Generator"
+pyton scripts/generator.py
 echo "Running Tunneling Script"
 python scripts/tunneling.py
